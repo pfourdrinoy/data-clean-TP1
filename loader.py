0be7409f33552d71106d3650a25bd83b5bd4e37d
pharaoph09 <1850904176@qq.com>
2024-03-16 12:50:38 +0100

    method

--- loader.py
+++ loader.py
@@ -94,16 +94,52 @@ def format_com_cp(df:pd.DataFrame) -> pd.DataFrame:
     return df
 
 import pandas as pd
+import numpy as np
 
-# 指定 CSV 文件路径
+#com_cp
 file_path = r'C:\Users\18509\Desktop\data-cleaning1\data-clean-TP1\data\sample_dirty.csv'
 
-# 读取 CSV 文件
+
 df = pd.read_csv(file_path)
 
-# 输出 com_cp 列的所有数据，如果是0则输出 pd.NA
 for value in df['com_cp']:
     if value == 0:
+        print(pd.NA)
+    else:
+        print(value)
+
+
+#long_coor1
+for value in df['long_coor1']:
+    if pd.isnull(value):  
+        print(np.NAN)
+    elif str(value).strip() == '':
+        print(np.NAN)
+    else:
+        print(value)
+
+#lat_coor1
+for value in df['lat_coor1']:
+    if pd.isnull(value): 
+        print(np.nan)
+    elif str(value).strip() == '' or str(value).strip() == '-':
+        print(np.nan)
+    else:
+        print(value)
+
+#dermnt
+for value in df['dermnt']:
+    date_value = pd.to_datetime(value, errors='coerce')  
+    if date_value == pd.Timestamp('1970-01-01'):  
+        print(pd.NaT)
+    else:
+        print(date_value.date())
+
+#freq_mnt
+for value in df['freq_mnt']:
+    if value == 0:
+        print(pd.NA)
+    elif str(value).strip() == '':
         print(pd.NA)
     else:
         print(value)
